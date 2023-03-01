c031d31dea95c68e98ff6967ca7c6879e69b43a0
JaviPatron <jpatronmf@gmail.com>
2023-02-28 17:06:46 -0800

    adding the map_shiny

---
+++ map_shiny.R
@@ -0,0 +1,61 @@
+library(tidyverse)
+library(raster)
+library(sp)
+library(tmap)
+library(sf)
+library(stars)
+library(gstat)
+library(rgdal)
+
+
+# read the .tiff files
+kelp_raster <- raster(here::here("demo_rasters", "kelp_raster.tiff"))
+depth_raster <- raster(here::here("demo_rasters", "depth_raster.tiff"))
+sst_raster <- raster(here::here("demo_rasters", "sst_raster.tiff"))
+NO3_raster <- raster(here::here("demo_rasters", "NO3_raster.tiff"))
+NO2_raster <- raster(here::here("demo_rasters", "NO2_raster.tiff"))
+NH4_raster <- raster(here::here("demo_rasters", "NH4_raster.tiff"))
+
+# Load the shape file for the Channel Islands
+channel_islands <- st_read(here::here("CI_data", "channel_islands.shp"))
+
+#Import the Data Frame
+area_sf <- readRDS(here::here("demo_rasters", "area_sf.rds"))
+
+# Plot world map, add the area of interest and the raster values
+data("World")
+
+tm_shape(World) +
+  tm_polygons() +
+  
+  tm_shape(channel_islands) +
+  tm_polygons()+
+  
+  tm_shape(area_sf) +
+  tm_borders(lwd = 1, col = "red") +
+  
+  tm_shape(kelp_raster) +
+  tm_raster(style = "fisher", palette = "BuGn", alpha = 0.8, title = "kelp") +
+  
+  tm_shape(depth_raster) +
+  tm_raster(style = "fisher", palette = "Blues", alpha = 0.8, title = "depth") +
+  
+  tm_shape(sst_raster) +
+  tm_raster(style = "fisher", palette = "YlOrRd", alpha = 0.8, title = "SST") +
+  
+  tm_shape(NO3_raster) +
+  tm_raster(style = "fisher", palette = "BuPu", alpha = 0.8, title = "NO3") +
+  
+  tm_shape(NO2_raster) +
+  tm_raster(style = "fisher", palette = "Greys", alpha = 0.8, title = "NO2") +
+  
+  tm_shape(NH4_raster) +
+  tm_raster(style = "fisher", palette = "PuRd", alpha = 0.8, title = "NH4") +
+  
+  tm_legend(title = "kelpGeoMod") +
+  tm_scale_bar(position = c("left", "bottom"), text.size = 0.7) +
+  tm_view(set.view = c(-120, 34.1, 8))
+
+tmap_mode("view")
+
+
--- timeseries_shiny.R
+++ timeseries_shiny.R
@@ -7,39 +7,12 @@ kelp_df <- readRDS(here::here("demo_rasters", "kelp_df.rds"))
 
 
 
-
-### 4. Print the variables as time series graphs
-#a)  In the same plot
-
-colors <- c("darkgreen", "#56B4E9", "orange", "purple", "#0072B2", "#D55E00")
-y_vars <- c("kelp_biomass", "depth", "sst", "NO3", "NO2", "NH4")
-
-# Create a plot for each variable with a buffer of 10% of the data range
-plot_list <- lapply(seq_along(colors), function(i) {
-  var <- y_vars[i]
-  y_range <- range(kelp_df[[var]])
-  buffer <- diff(y_range) * 0.2
-  ggplot(kelp_df, aes(x = date, y = !!sym(var))) +
-    geom_line(color = colors[i]) +
-    labs(x = "Date", y = var) +
-    ylim(y_range + c(-buffer, buffer))
-})
-
-# Combine the plots into a single figure using facet_wrap
-do.call(gridExtra::grid.arrange, c(plot_list, ncol = 2))
-
-
-#b)  Print them in six different graphs
-# Create a for loop to generate a separate ggplot for each y variable
-
-for (i in seq_along(y_vars)) {
-  
-  # Create the ggplot
-  final_plots <- ggplot(kelp_df, aes(x = date, y = !!sym(y_vars[i]))) +
-    geom_line(color = colors[i]) +
-    labs(title = y_vars[i], y = y_vars[i], x = "Date")
-  
-  # Print the ggplot
-  print(final_plots)
-  
-}
+#Print the time series
+ggplot(kelp_df, aes(x = date, y = kelp_biomass)) +
+  geom_line(color = "darkolivegreen") + 
+  theme_classic() +
+  labs(title = "Kelp Biomass Time Series",
+       subtitle = "Santa Barbara Channel",
+       x = "Date",
+       y = "Kelp Biomass (kg)",
+       caption = "data source: LTER")
